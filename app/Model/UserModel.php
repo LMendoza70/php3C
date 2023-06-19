0d076fcba988f8aa9ee6ee6a45fd4430d3ad4230
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-06-19 13:59:54 -0600

    modified:   app/Model/UserModel.php

--- app/Model/UserModel.php
+++ app/Model/UserModel.php
@@ -1,5 +1,38 @@
 <?php
     class UserModel{
         //creamos un atributo para manipular los datos en la bd
+        private $UserConnection;
+
+        //definimos el contructor de la clase usermodel
+        public function __construct(){
+            //requiero la clase dbconnection 
+            require_once('../config/DBConnection.php');
+            //instranciamos userconnection con dbconnection 
+            $this->UserConnection=new DBConnection();
+        }
+
+        //a partir de esto vienen los metodos logicos de la app
+
+        //metodo para obtener todos los usuarios
+        public function getAll(){
+            //paso 1 creo la consulta
+            $sql="SELECT * FROM user";
+            //paso 2 llamo a la conneccion 
+            $connection =$this->UserConnection->getconnection();
+            //paso 3 ejecuto la consulta
+            $result=$connection->query($sql);
+            //paso 4 verifico y acomodo datos 
+            //paso 4.1 creo un arreglo para almacenar los usuarios de la bd 
+            $users=array();
+            //tengo que recorrer $result para ir extrayendo los renglones (registros de usuarios)
+            //ocupare un while y la instruccion fetch_assoc
+            while($user=$result->fetch_assoc()){
+                $users[]=$user;
+            }
+            //paso 5 cierro la coneccion 
+            $this->UserConnection->closeConnection();
+            //paso 6 arrojo resultados
+            return $users;
+        }
     }
 ?>
