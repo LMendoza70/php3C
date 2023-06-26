c495ed59692dd8ef82ae73098c104b98f31ed130
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-06-26 13:51:16 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   app/Controller/DefaultController.php
    	renamed:    app/View/registerView.php -> app/Controller/UserController.php
    	deleted:    app/Controller/loginController.php
    	new file:   app/View/IndexAdminView.php
    	new file:   app/View/PlantillaAdminView.php
    	renamed:    public/css/styles.css -> app/View/PlantillaPublicView.php
    	renamed:    public/index.html -> app/View/indexPublicView.php
    	deleted:    app/View/loginView.html
    	new file:   app/View/style.css
    	new file:   index.php

---
+++ app/View/PlantillaAdminView.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Catando Ando Coffee Shop</title>
+    <link rel="stylesheet" href="app/View/style.css" />
+  </head>
+  <body>
+    <header class="header">
+      <h1>Catando Ando Coffee Shop</h1>
+      <nav class="navbar">
+        <ul>
+          <li><a href="">Inicio</a></li>
+          <li><a href="">Productos</a></li>
+          <li><a href="">Presentacion</a></li>
+          <li><a href="http://localhost/php3c?C=UserController&M=Index">Usuarios</a></li>
+          <li><a href="">Variedad</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section class="content">
+      <!--Aqui va a ir todo lo que querramos mostrar dentro de la plantilla -->
+      <?php include_once($vista); ?>
+    </section>
+    <footer class="footer">
+      <h3>Derechos reservados ...</h3>
+    </footer>
+  </body>
+</html>
